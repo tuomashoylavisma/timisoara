57ebdc672b522bca2d3a389b3638176457e8a0db
Tuomas <tuomas.hoyla@gmail.com>
2019-10-31 21:39:46 +0200

    changed to use hooks

--- src/Tree.js
+++ src/Tree.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import PropTypes from 'prop-types'
+import convertToTree from './treeConverter'
 
-const NodeRow = ({node, selectNode}) => <div id={node.id} onClick={() => selectNode(node.name)}>{node.name}</div>
+const TREE_URL = `https://grim-spider-21894.herokuapp.com/`
+
+const NodeRow = ({node, selectNode}) => <div id={node.id} onClick={() => selectNode(node)}>{node.name}</div>
 
 const DrawTree = ({branch, selectNode}) => {
 
@@ -17,11 +20,26 @@ if (branch.children.length > 0) {
     return <NodeRow node={branch} selectNode={selectNode} />
 }
 
-const Tree = ({selectNode, tree, selectedNode}) => {
+const Tree = ({}) => {
+
+    const [selectedNode, selectNode] = useState(null);
+    const [tree, setTree] = useState([]);
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(TREE_URL)
+          .then(res => res.json())
+          .then(response => {
+              setTree(convertToTree(response))
+              setLoading(false)
+          })
+          .catch(error => console.log(error));
+      })
 
     return (<div>
+        {isLoading && <p>Loading....</p>}
         {tree.map(branch => <DrawTree id={branch.id} branch={branch} selectNode={selectNode} />)}
-        {selectedNode && <div style={{fontSize: '0.8em', paddingTop: '14px', color: 'yellow'}}>Selected: {selectedNode}</div>}
+        {selectedNode && <div style={{fontSize: '0.8em', paddingTop: '14px', color: 'yellow'}}>Selected: {selectedNode.name}</div>}
        </div>
     )
 }
